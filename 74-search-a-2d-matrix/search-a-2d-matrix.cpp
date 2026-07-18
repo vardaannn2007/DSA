56907f3a449a8af7ee36b46ef9fef61ba454d74c
Vardan Gupta <vg875306@gmail.com>
2026-07-18 13:36:25 +0530

    Time: 3 ms (1.12%) | Memory: 13.6 MB (7.43%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -3,12 +3,13 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix.size();
         int n= matrix[0].size();
-
-        for(int i=0; i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==target) return true;
+        vector<int> flat;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                flat.push_back(matrix[i][j]);
             }
         }
+        if(binary_search(flat.begin(), flat.end(), target)) return true;
         return false;
     }
 };

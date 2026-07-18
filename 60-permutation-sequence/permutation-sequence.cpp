dc0008c7b224cd0c27dbd3c9b704db0ece4bd7d1
Vardan Gupta <vg875306@gmail.com>
2026-07-18 15:05:19 +0530

    Time: 55 ms (17.06%) | Memory: 7.6 MB (99.78%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s = "";
+        for (int i = 1; i <= n; i++)
+            s += char(i + '0');
+        for (int i = 1; i < k; i++)
+            next_permutation(s.begin(), s.end());
+        return s;
+    }
+};

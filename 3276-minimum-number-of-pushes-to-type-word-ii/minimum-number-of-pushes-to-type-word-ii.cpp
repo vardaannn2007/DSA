5e9b3fcef5ac133347a78ff882d4547dd0382cf8
Vardan Gupta <vg875306@gmail.com>
2026-07-30 12:50:02 +0530

    Time: 29 ms (25.97%) | Memory: 26.9 MB (46.20%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        unordered_map<char, int> mp;
+        for (char c : word)
+            mp[c]++;
+        vector<int> freq;
+        for (auto p : mp)
+            freq.push_back(p.second);
+        sort(freq.begin(), freq.end(), greater<int>());
+        int ans = 0;
+        for (int i = 0; i < freq.size(); i++) {
+            ans += freq[i] * ((i / 8) + 1);
+        }
+        return ans;
+    }
+};
